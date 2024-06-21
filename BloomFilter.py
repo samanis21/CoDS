72cf9ada7ec7ae9e283438b7f27838837fc5d434
samanis21 <soumaya.kasri@student.uhasselt.be>
2024-06-21 14:54:08 +0200

    refactor and Use hashlib for hashing in BloomFilter instead of mmh3(didn't work in HPC)
    - Replaced mmh3 with hashlib for generating hash values.
    - Updated _hashes method to use SHA-256 and MD5 for hash generation.

--- BloomFilter.py
+++ BloomFilter.py
@@ -1,32 +1,35 @@
-from BitSet import BitSet
-import mmh3
+import hashlib
+import math
+from Bitset import Bitset
 
 class BloomFilter:
-    def __init__(self, capacity, error_rate=0.01):
-        self.size = self._optimal_size(capacity, error_rate)
-        self.hash_count = self._optimal_hash_count(capacity, error_rate)
-        self.bitset = BitSet(self.size)
+    def __init__(self, capacity, error_rate):
+        self.capacity = capacity
+        self.error_rate = error_rate
+        self.size = self._get_size(capacity, error_rate)
+        self.num_hashes = self._get_num_hashes(capacity, error_rate)
+        self.bitset = Bitset(self.size)
+
+    def _hashes(self, item):
+        """Generate multiple hash values for the given item."""
+        hash1 = int(hashlib.sha256(item.encode('utf-8')).hexdigest(), 16)
+        hash2 = int(hashlib.md5(item.encode('utf-8')).hexdigest(), 16)
+        for i in range(self.num_hashes):
+            yield (hash1 + i * hash2) % self.size
 
     def add(self, item):
-        item = str(item)
-        for i in range(self.hash_count):
-            combined_hash = mmh3.hash(item, i) % self.size
-            self.bitset.add(combined_hash)
+        for pos in self._hashes(item):
+            self.bitset.set(pos)
 
     def contains(self, item):
-        item = str(item)
-        for i in range(self.hash_count):
-            combined_hash = mmh3.hash(item, i) % self.size
-            if not self.bitset.contains(combined_hash):
-                return False
-        return True
+        return all(self.bitset.get(pos) for pos in self._hashes(item))
 
     @staticmethod
-    def _optimal_size(capacity, error_rate):
-        from math import ceil, log
-        return ceil(-(capacity * log(error_rate)) / (log(2) ** 2))
+    def _get_size(capacity, error_rate):
+        m = -(capacity * math.log(error_rate)) / (math.log(2) ** 2)
+        return int(m)
 
     @staticmethod
-    def _optimal_hash_count(capacity, error_rate):
-        from math import log
-        return round((BloomFilter._optimal_size(capacity, error_rate) / capacity) * log(2))
+    def _get_num_hashes(capacity, error_rate):
+        k = (math.log(2) * BloomFilter._get_size(capacity, error_rate)) / capacity
+        return int(k)
