3682083d04ed8819d52bbf6d33d02ee23f95adb6
samanis21 <soumaya.kasri@student.uhasselt.be>
2024-06-22 13:55:07 +0200

    Optimize the Bitset class: - Added input validation to `set()`, `get()`, and `clear()` methods to handle out-of-range indices - Added a `clear()` method to set a bit to 0 - Added a `__str__()` method to display the bitset as a string

--- BitSet.py
+++ BitSet.py
@@ -4,7 +4,19 @@ class Bitset:
         self.bitset = [0] * size
 
     def set(self, pos):
+        if pos < 0 or pos >= self.size:
+            raise IndexError("Bitset index out of range")
         self.bitset[pos] = 1
 
     def get(self, pos):
+        if pos < 0 or pos >= self.size:
+            raise IndexError("Bitset index out of range")
         return self.bitset[pos]
+
+    def clear(self, pos):
+        if pos < 0 or pos >= self.size:
+            raise IndexError("Bitset index out of range")
+        self.bitset[pos] = 0
+
+    def __str__(self):
+        return ''.join(map(str, self.bitset))
