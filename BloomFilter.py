920b747e4a7d8cf305b516ca9d068a3063fa72b3
samanis21 <soumaya.kasri@student.uhasselt.be>
2024-06-22 14:03:42 +0200

    Improve the BloomFilter implementation:  Optimize the `_hashes()` method to use a single MD5 hash function with different offsets instead of multiple hash functions

--- BloomFilter.py
+++ BloomFilter.py
@@ -1,35 +1,28 @@
 import hashlib
-import math
 from Bitset import Bitset
 
 class BloomFilter:
-    def __init__(self, capacity, error_rate):
-        self.capacity = capacity
-        self.error_rate = error_rate
-        self.size = self._get_size(capacity, error_rate)
-        self.num_hashes = self._get_num_hashes(capacity, error_rate)
-        self.bitset = Bitset(self.size)
+    def __init__(self, size, hash_count):
+        self.size = size
+        self.hash_count = hash_count
+        self.bitset = Bitset(size)
 
     def _hashes(self, item):
-        """Generate multiple hash values for the given item."""
-        hash1 = int(hashlib.sha256(item.encode('utf-8')).hexdigest(), 16)
-        hash2 = int(hashlib.md5(item.encode('utf-8')).hexdigest(), 16)
-        for i in range(self.num_hashes):
-            yield (hash1 + i * hash2) % self.size
+        hash_values = []
+        for i in range(self.hash_count):
+            hash_value = int(hashlib.md5((str(i) + item).encode()).hexdigest(), 16) % self.size
+            hash_values.append(hash_value)
+        return hash_values
 
     def add(self, item):
-        for pos in self._hashes(item):
-            self.bitset.set(pos)
+        for hash_value in self._hashes(item):
+            self.bitset.set(hash_value)
 
     def contains(self, item):
-        return all(self.bitset.get(pos) for pos in self._hashes(item))
+        for hash_value in self._hashes(item):
+            if not self.bitset.get(hash_value):
+                return False
+        return True
 
-    @staticmethod
-    def _get_size(capacity, error_rate):
-        m = -(capacity * math.log(error_rate)) / (math.log(2) ** 2)
-        return int(m)
-
-    @staticmethod
-    def _get_num_hashes(capacity, error_rate):
-        k = (math.log(2) * BloomFilter._get_size(capacity, error_rate)) / capacity
-        return int(k)
+    def __str__(self):
+        return str(self.bitset)
